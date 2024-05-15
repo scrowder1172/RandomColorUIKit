7fe37586d8ac6670b4258f907aa6c84097307f91
Scott Crowder <scrowder@gmail.com>
2024-05-15 15:08:52 -0500

    generate random colors and display in table view. Enable tap on table view to load view that shows color full screen. refactor strings into enum for safety.

--- RandomColorUIKit/ColorsTableVC.swift
+++ RandomColorUIKit/ColorsTableVC.swift
@@ -8,29 +8,56 @@
 import UIKit
 
 class ColorsTableVC: UIViewController {
+    
+    var colors: [UIColor] = []
+    
+    enum Cells {
+        static let colorCell = "ColorCell"
+    }
+    
+    enum Segues {
+        static let toDetail = "ToColorsDetailVC"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        createRandomColors()
     }
     
+    func createRandomColors() {
+        for _ in 0..<50 {
+            colors.append(UIColor.random())
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destVC = segue.destination as! ColorsDetailVC  // create a destination of type ColorsDetailVC
+        destVC.color = sender as? UIColor
+    }
 }
 
 extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50  // how many rows will be in the tableView
+//        return 50  // how many rows will be in the tableView
+        return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // this function runs whenever a new cell appears on the screen
         
-        return UITableViewCell()  // return blank table view cell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Cells.colorCell) else { return UITableViewCell() }
+        let selectedColor = colors[indexPath.row]
+        cell.backgroundColor = selectedColor
+        
+//        return UITableViewCell()  // return blank table view cell
+        return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)  // navigate to color screen when cell is tapped
+        let color = colors[indexPath.row]  // get color for row tapped
+        performSegue(withIdentifier: Segues.toDetail, sender: color)  // navigate to color screen when cell is tapped
     }
     
 }
+
